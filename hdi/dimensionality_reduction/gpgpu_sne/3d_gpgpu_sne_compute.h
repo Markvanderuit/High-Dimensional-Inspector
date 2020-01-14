17bbce9303212e9bf487ddcd04c16720a4be3e90
Mark van de Ruit <markvanderuit@gmail.com>
2020-01-14 15:21:03 +0100

    Added proper license header to new files

--- hdi/dimensionality_reduction/gpgpu_sne/3d_gpgpu_sne_compute.h
+++ hdi/dimensionality_reduction/gpgpu_sne/3d_gpgpu_sne_compute.h
@@ -1,10 +1,34 @@
-#ifndef __APPLE__
+/*
+ * Copyright (c) 2014, Nicola Pezzotti (Delft University of Technology)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *  must display the following acknowledgement:
+ *  This product includes software developed by the Delft University of Technology.
+ * 4. Neither the name of the Delft University of Technology nor the names of
+ *  its contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NICOLA PEZZOTTI ''AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL NICOLA PEZZOTTI BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
 
 #pragma once
-
-#define ASSERT_SUM_Q // Enable SUM_Q value checking
-#define USE_DEPTH_FIELD // Enable depthmap leveraging field computation
-// #define USE_MIPMAP // Enable mipmap leveraging field computation
 
 #include <cstdlib>
 #include "hdi/utils/abstract_log.h"
@@ -12,6 +36,10 @@
 #include "hdi/dimensionality_reduction/tsne_parameters.h"
 #include "gpgpu_utils.h"
 #include "3d_utils.h"
+
+#define ASSERT_SUM_Q // Enable SumQ != 0 assertion
+#define USE_DEPTH_FIELD // Field computation leverages depth maps
+// #define USE_MIPMAP // Field computation leverages mipmaps
 
 #ifdef USE_DEPTH_FIELD
 #include "depth_field_computation.h"
@@ -117,5 +145,3 @@ namespace hdi::dr {
     )
   };
 }
-
-#endif
